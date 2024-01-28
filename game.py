efb6862d7d31e99c324c4746a62b7112358f4644
joeyissa <joeyissa59@gmail.com>
2024-01-28 00:30:28 -0500

    cleaned up some code

---
+++ game.py
@@ -0,0 +1,290 @@
+import sys
+import os
+import math
+import random
+import pygame
+
+from scripts.utils import load_image, load_images, Animation
+from scripts.entities import PhysicsEntity, Player, Enemies
+from scripts.tilemap import Tilemap
+from scripts.particle import Particle
+from scripts.spark import Spark
+from scripts.UI import Heart, Levelbar
+
+class Game:
+    def __init__(self):
+        '''
+        initializes Game
+        '''
+        pygame.init()
+
+        # change the window caption
+        pygame.display.set_caption("10 Levels of Hell")
+        # create window
+        self.screen = pygame.display.set_mode((640, 480)) # (640, 480), (960, 720), (768, 576)
+
+        self.display_red= pygame.Surface((320, 240), pygame.SRCALPHA) # render on smaller resolution then scale it up to bigger screen
+
+        self.display_white = pygame.Surface((320, 240), pygame.SRCALPHA) # render on smaller resolution then scale it up to bigger screen
+        self.display_black = pygame.Surface((320, 240), pygame.SRCALPHA) # render on smaller resolution then scale it up to bigger screen
+        self.display_none = pygame.Surface((320, 240), pygame.SRCALPHA) # render on smaller resolution then scale it up to bigger screen
+
+        self.display_2 = pygame.Surface((320, 240))
+
+
+        self.clock = pygame.time.Clock()
+        
+        self.movement = [False, False, False, False]
+
+        self.assets = {
+            'player': load_image('entities/player/player.png'),
+            'background': load_image('background.png'),
+            'heart': load_image('UI/health.png'),
+
+            'player/idle': Animation(load_images('entities/player/idle'), img_dur=1),
+            'player/shoot': Animation(load_images('entities/player/run'), img_dur=4),
+            'particle/particle': Animation(load_images('particles/particle'), img_dur=6, loop=False),
+        }
+
+        # adding sound
+        self.sfx = {
+            'jump': pygame.mixer.Sound('data/sfx/jump.wav'),
+            'dash': pygame.mixer.Sound('data/sfx/dash.wav'),
+            'hit': pygame.mixer.Sound('data/sfx/hit.wav'),
+            'shoot': pygame.mixer.Sound('data/sfx/shoot.wav'),
+            'ambience': pygame.mixer.Sound('data/sfx/ambience.wav'),
+        }
+        
+        self.sfx['ambience'].set_volume(0.2)
+        self.sfx['shoot'].set_volume(0.4)
+        self.sfx['hit'].set_volume(0.8)
+        self.sfx['dash'].set_volume(0.3)
+
+        #self.clouds = Clouds(self.assets['clouds'], count=16)
+
+        # initalizing player
+        self.player = Player(self, (self.display_red.get_width()/2, self.display_red.get_height()/2), (15, 15))
+
+        # initalizing tilemap
+        self.tilemap = Tilemap(self, tile_size=16)
+
+        # tracking level
+        self.level = 0
+        self.max_level = len(os.listdir('data/maps')) # max level,
+        # loading the level
+        self.load_level(0)  # self.load_level(self.level), hard coding to 1 atm
+
+        # screen shake
+        self.screenshake = 0
+
+        self.cooldown = 0
+        self.angle_count = 0
+        self.horizontal_count = 0
+
+
+    def load_level(self, map_id):
+        self.tilemap.load('data/maps/' + str(map_id) + '.json')
+
+        # keep track
+        self.particles = []
+
+        # creating 'camera' 
+        self.scroll = [0, 0]
+
+        self.dead = -2  # gives player 3 lives, -2, -1, 0
+
+        # transition for levels
+        self.transition = -30
+        
+        # spawn the ememies
+        self.enemies = [] # make this random generate and increase spawn # as game time goes on
+
+        for spawner in self.tilemap.extract([('spawners', 0), ('spawners', 1), ('spawners', 2), ('spawners', 3)]):
+            if spawner['variant'] == 0: 
+                self.player.pos = spawner['pos']
+            else:
+                self.enemies.append(enemies(self, spawner['pos'], (21, 31)))
+                # spawn the ememies
+
+    def run(self):
+        '''
+        runs the Game
+        '''
+        pygame.mixer.music.load('data/music.mp3')
+        pygame.mixer.music.set_volume(0.5)
+        pygame.mixer.music.play(-1)
+
+        self.sfx['ambience'].play(-1)
+
+        # creating an infinite game loop
+        while True:
+            self.display_red.fill((0, 0, 0, 0))    # red outlines
+            self.display_white.fill((0, 0, 0, 0))    # white outlines
+            self.display_black.fill((0, 0, 0, 0))    # black outlines
+            self.display_none.fill((0,0,0,0))
+            # clear the screen for new image generation in loop
+            self.display_2.blit(self.assets['background'], (0,0)) # no outline
+
+            self.screenshake = max(0, self.screenshake-1) # resets screenshake value
+
+            if self.dead >= 1: # get hit 3 times
+                self.dead += 1
+                if self.dead >= 10: # to make the level transitions smoother
+                    self.transition = min(self.transition + 1, 30) # go as high as it can without changing level
+                if self.dead > 40: # timer that starts when you die
+                    # self.level = 0
+                    self.load_level(self.level) # start at level 0 again. self.load_level(0)
+            
+
+            # scroll = current scroll + (where we want the camera to be - what we have/can see currently) 
+            self.scroll[0] = self.display_red.get_width()/2 / 30 + 3 # x axis
+            self.scroll[1] = self.display_red.get_height()/2/ 30 + 3
+
+            # fix the jitter
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            self.tilemap.render(self.display_white, offset=render_scroll)
+
+            # render the enemies
+            for enemy in self.enemies.copy():
+                kill =  enemy.update(self.tilemap, (0,0))
+                enemy.render(self.display_black, offset=render_scroll) # change outline here
+                if kill: # if enemies update fn returns true [**]
+                    self.enemies.remove(enemy) 
+                if self.player.rect().colliderect(enemy): # player collides with enemy
+                    self.dead += 1 # die
+                    self.sfx['hit'].play()
+                    self.cooldown = 150
+                    self.screenshake = max(16, self.screenshake)  # apply screenshake, larger wont be overrided by a smaller screenshake
+                    for i in range(30): # when projectile hits player
+                        # on death sparks
+                        angle = random.random() * math.pi * 2 # random angle in a circle
+                        speed = random.random() * 5
+                        self.sparks.append(Spark(self.player.rect().center, angle, 2 + random.random()))    # fix sparks
+                        # on death particles
+                        self.particles.append(Particle(self, 'particle', self.player.rect().center, velocity=[math.cos(angle + math.pi) * speed * 0.5, math.sin(angle * math.pi) * speed * 0.5], frame=random.randint(0, 7)))
+
+
+            # Reduce timer
+            if self.cooldown > 0:
+                    self.cooldown -= 1
+
+            if self.dead != 1:
+                # update player movement
+                self.player.update(self.tilemap, (self.movement[1] - self.movement[0], self.movement[3] - self.movement[2]))
+                self.player.render(self.display_white, offset=render_scroll)
+
+            # render/spawn bullet projectiles
+            # [[x, y], direction, timer]
+            # make direction go based on angle
+            for projectile in self.projectiles.copy():
+                projectile[0][0] += projectile[1] 
+                projectile[2] += 1
+                img = self.assets['projectile']
+                self.display_black.blit(img if projectile[1] > 0 else pygame.transform.flip(img, True, False), (projectile[0][0] - img.get_width() / 2 - render_scroll[0], projectile[0][1] - img.get_height() / 2 - render_scroll[1])) # spawns it the center of the projectile
+                
+                # keep this but change it to the borders of the map, also might want some obsticles later
+                if self.tilemap.solid_check(projectile[0]): # if location is a solid tile
+                    self.projectiles.remove(projectile)
+                    for i in range(4):
+                        self.sparks.append(Spark(projectile[0], random.random() - 0.5 + (math.pi if projectile[1] > 0 else 0), 2 + random.random())) # (math.pi if projectile[1] > 0 else 0), sparks bounce in oppositie direction if hit wall which depends on projectile direction
+                elif projectile[2] > 360: #if timer > 6 seconds
+                    self.projectiles.remove(projectile)
+                elif abs(self.player.dashing) < 50: # if not in dash
+                    if self.player.rect().collidepoint(projectile[0]):
+                        self.projectiles.remove(projectile)
+                        self.dead += 1
+                        self.sfx['hit'].play()
+                        self.screenshake = max(16, self.screenshake)  # apply screenshake, larger wont be overrided by a smaller screenshake
+                        for i in range(30): # when projectile hits player
+                            # on death sparks
+                            angle = random.random() * math.pi * 2 # random angle in a circle
+                            speed = random.random() * 5
+                            self.sparks.append(Spark(self.player.rect().center, angle, 2 + random.random())) 
+                            # on death particles
+                            self.particles.append(Particle(self, 'particle', self.player.rect().center, velocity=[math.cos(angle + math.pi) * speed * 0.5, math.sin(angle * math.pi) * speed * 0.5], frame=random.randint(0, 7)))
+           
+                                    
+            hp_1 = Heart(self.assets['heart'].copy(), [13, 19], 15)
+            hp_2 = Heart(self.assets['heart'].copy(), [30, 19], 15)
+            hp_3 = Heart(self.assets['heart'].copy(), [47, 19], 15)
+            if self.dead <= 0 and self.dead < 1:
+                hp_1.update()
+                hp_1.render(self.display_black)
+            if self.dead <= -1:
+                hp_2.update()
+                hp_2.render(self.display_black)
+            if self.dead <= -2:
+                hp_3.update()
+                hp_3.render(self.display_black)
+
+            level_bar = Levelbar(self.level, pos=(self.display_red.get_width() // 2 - 25, 13))
+            level_bar.render(self.display_black, 22)
+            
+
+            # black ouline based on display_black
+            display_mask = pygame.mask.from_surface(self.display_black)
+            display_sillhouette = display_mask.to_surface(setcolor=(0, 0, 0, 180), unsetcolor=(0, 0, 0, 0)) # 180 opaque, 0 transparent
+            self.display_2.blit(display_sillhouette, (0, 0))
+            for offset in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                self.display_2.blit(display_sillhouette, offset) # putting what we drew onframe back into display
+
+            # red ouline based on display
+            display_mask = pygame.mask.from_surface(self.display_red)
+            display_sillhouette = display_mask.to_surface(setcolor=(225, 0, 0, 180), unsetcolor=(0, 0, 0, 0)) # 180 opaque, 0 transparent
+            self.display_2.blit(display_sillhouette, (0, 0))
+            for offset in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                self.display_2.blit(display_sillhouette, offset) # putting what we drew onframe back into display
+            
+
+            # white ouline based on display_white
+            display_mask = pygame.mask.from_surface(self.display_white)
+            display_sillhouette = display_mask.to_surface(setcolor=(225, 225, 225, 180), unsetcolor=(0, 0, 0, 0)) # 180 opaque, 0 transparent
+            self.display_2.blit(display_sillhouette, (0, 0))
+            for offset in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                self.display_2.blit(display_sillhouette, offset) # putting what we drew onframe back into display
+            
+
+            for particle in self.particles.copy():
+                kill = particle.update()
+                particle.render(self.display_red, offset=render_scroll)
+                if particle.type == 'leaf':
+                    particle.pos[0] += math.sin(particle.animation.frame * 0.035) * 0.3 # making the parlitcle move back and forth smooth'y
+                if kill:
+                    self.particles.remove(particle)
+
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: # have to code the window closing
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_a: # referencing WASD
+                        self.movement[0] = True
+                    if event.key == pygame.K_d:
+                        self.movement[1] = True # make going forward alway constant
+                if event.type == pygame.KEYUP: # when key is released
+                    if event.key == pygame.K_a: 
+                        self.movement[0] = False
+                    if event.key == pygame.K_d: 
+                        self.movement[1] = False
+            
+            self.display_2.blit(self.display_white, (0, 0)) # white
+            self.display_2.blit(self.display_red, (0, 0)) # red 
+            self.display_2.blit(self.display_black, (0, 0)) # black 
+            self.display_2.blit(self.display_none, (0,0))
+            
+            # implementing transition
+            if self.transition:
+                transition_surf = pygame.Surface(self.display_red.get_size())
+                pygame.draw.circle(transition_surf, (255, 255, 255), (self.display_red.get_width() // 2, self.display_red.get_height() // 2), (30 - abs(self.transition)) * 8) # display center of screen, 30 is the timer we chose, 30 * 8 = 180
+                transition_surf.set_colorkey((255, 255, 255)) # making the circle transparent now
+                self.display_2.blit(transition_surf, (0, 0))
+
+            screenshake_offset = (random.random() * self.screenshake - self.screenshake / 2, random.random() * self.screenshake - self.screenshake / 2)
+            self.screen.blit(pygame.transform.scale(self.display_2, self.screen.get_size()), screenshake_offset) # render (now scaled) display image on big screen
+            pygame.display.update()
+            self.clock.tick(60) # run at 60 fps, like a sleep
+
+# returns the game then runs it
+Game().run()
---
+++ scripts/entities.py
@@ -0,0 +1,267 @@
+import pygame
+import math
+import random
+
+from scripts.particle import Particle
+from scripts.spark import Spark
+from scripts.UI import Heart
+
+class PhysicsEntity:
+    def __init__(self, game, e_type, pos, size):
+        '''
+        initializes entities
+        (game, entitiy type, position, size)
+        '''
+        self.game = game
+        self.type = e_type
+        self.pos = list(pos) #make sure each entitiy has it's own list, (x,y)
+        self.size = size
+        self.velocity = [0,0]
+        self.collisions = {'up': False, 'down': False, 'left': False, 'right': False}
+
+        self.action = ''
+        self.anim_offset = (-3, -3) #renders with an offset to pad the animation against the hitbox
+        self.flip = False
+        
+        self.set_action('idle') #**
+
+        self.last_movement = [0, 0]
+
+    def rect(self):
+        '''
+        creates a rectangle at the entitiies current postion
+        '''
+        return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+    
+    def set_action(self, action):
+        '''
+        sets a new action to change animation
+        (string of animation name) -> animation
+        '''
+        if action != self.action: # if action has changed
+            self.action = action
+            self.animation = self.game.assets[self.type + '/' + self.action].copy()
+
+
+    
+    def update(self, tilemap, movement=(0,0)):
+        '''
+        updates frames and entitiy position 
+        '''
+        self.collisions = {'up': False, 'down': False, 'left': False, 'right': False} # this value will be reset every frame, used to stop constant increase of velocity
+
+        frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+        
+        self.pos[0] += frame_movement[0]
+        entity_rect = self.rect() # getting the entities rectange
+        # move tile based on collision on y axis
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[0] > 0: # if moving right and you collide with tile
+                    entity_rect.right = rect.left
+                    self.collisions['right'] = True
+                if frame_movement[0] < 0: # if moving left
+                    entity_rect.left = rect.right
+                    self.collisions['left'] = True
+                self.pos[0] = entity_rect.x
+        
+        # Note: Y-axis collision handling comes after X-axis handling
+        self.pos[1] += frame_movement[1]
+        entity_rect = self.rect()  # Update entity rectangle for y-axis handling
+        # move tile based on collision on y axis
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[1] > 0: # if moving right and you collide with tile
+                    entity_rect.bottom = rect.top
+                    self.collisions['down'] = True
+                if frame_movement[1] < 0: # if moving left
+                    entity_rect.top = rect.bottom
+                    self.collisions['up'] = True
+                self.pos[1] = entity_rect.y
+
+        entity_rect = self.rect()  # Update entity rectangle for y-axis handling
+
+        # find when to flip img for animation
+        if movement[0] > 0:
+            self.flip = False
+        if movement[0] < 0:
+            self.flip = True
+
+        self.last_movement = movement # keeps track of movement
+
+        self.animation.update() # update animation
+
+
+    def render(self, surf, offset={0,0}):
+        '''
+        renders entitiy asset
+        '''
+        surf.blit(pygame.transform.flip(self.animation.img(), self.flip, False), (self.pos[0] - offset[0] + self.anim_offset[0], self.pos[1] - offset[1] + self.anim_offset[1])) # fliping agasint horizontal axis
+
+
+
+class Player(PhysicsEntity):
+    def __init__(self, game, pos, size):
+        '''
+        instantiates player entity
+        (game, position, size)
+        '''
+        super().__init__(game,'player', pos, size)
+        self.dashing = 0
+
+    def update(self, tilemap, movement=(0,0)):
+        '''
+        updates players animations depending on movement
+        '''
+        super().update(tilemap, movement=movement)
+
+        if abs(self.dashing) in (60, 50): # if at start or end of dash
+            for i in range(20): # do 20 times
+                # for burst of particles
+                angle = random.random() * math.pi * 2
+                speed = random.random() * 0.5 + 0.5 # random from 0.5 to 1
+                pvelocity = [math.cos(angle) * speed, math.sin(angle) * speed]
+                self.game.particles.append(Particle(self.game, 'particle', self.rect().center, velocity=pvelocity, frame=random.randint(0, 7)))
+        # dash cooldown
+        if self.dashing > 0:
+            self.dashing = max(0, self.dashing - 1)
+        if self.dashing < 0:
+            self.dashing = min(0, self.dashing + 1)
+        # dash mechanism (only works in first 10 frames of 'dashing')
+
+        # Will have to change to go in the direction of the mouse
+        if abs(self.dashing) > 50:
+            # need where we want to dash towards
+            if self.direction == 'L': # Left
+                self.velocity[0] = 8 
+            if self.direction == 'J':
+                self.velocity[0] = -8 
+            if self.direction == 'I':
+                 self.velocity[1] = -8 
+            if self.direction == 'K':
+                 self.velocity[1] = 8        
+            if abs(self.dashing) == 51: # slow down dash after 10 frames
+                self.velocity[0] *= 0.01
+                self.velocity[1] *= 0.01  # goes to 0, but never allows player to move downward
+            # trail of particles in the middle of dash
+            pvelocity = [abs(self.dashing)/self.dashing * random.random() * 3, 0] # particles move in the direction of the dash
+            self.game.particles.append(Particle(self.game, 'particle', self.rect().center, velocity=pvelocity, frame=random.randint(0, 7)))
+    
+        if movement[0] != 0: # if moving horizontally
+            self.set_action('run')
+        elif movement[1] > 0:
+            self.set_action('runDOWN')
+        elif movement[1] < 0:
+            self.set_action('runUP')
+        else:
+            self.set_action('idle')
+
+        if abs(self.velocity[0]) < 0.1: # stops small sliding across screen after dash
+            self.velocity[0] = 0
+        if abs(self.velocity[1]) < 0.1:
+            self.velocity[1] = 0
+
+
+    def render(self, surf, offset={0,0}):
+        '''
+        partly overriding rendering for dashing
+        '''
+        if abs(self.dashing) <= 50: # not in first 10 frames of dash
+            super().render(surf, offset=offset) # show player
+
+    
+    def dash(self, direction):
+        '''
+        makes the player dash
+        (direction) 'I' for up, 'J'/'L' for right/left, 'K' for down
+        '''
+        self.direction = direction
+        if not self.dashing:
+            self.game.sfx['dash'].play()
+            if self.flip:
+                self.dashing = -60
+            else:
+                self.dashing = 60 # how long the dash is + it's direction
+                self.set_action('slide')
+            
+
+class Enemies(PhysicsEntity):
+    def __init__(self, game, pos, size):
+        '''
+        instantiates the enemies
+        (game, position: tuple, size)
+        '''
+        super().__init__(game, 'skele', pos, size)
+        self.walking = 1
+        self.speed = 1 # enemy speed
+        self.timer = 0 # enemy shooting timer
+    
+    def update(self, tilemap, movement=(0,0)):
+        if self.walking:
+            # Using the distance formula
+            dis = pygame.math.Vector2(self.game.player.pos[0] - self.pos[0], self.game.player.pos[1] - self.pos[1])
+            distance = dis.length()            
+            if distance >= 30:
+                angle = math.atan2(dis.y, dis.x)
+                movement = (math.cos(angle) * self.speed, math.sin(angle) * self.speed)
+                self.walking = True
+            elif (abs(self.game.player.pos[1] - self.pos[1]) != 0):
+                angle = math.atan2(dis.y, dis.x)
+                movement = (0, math.sin(angle) * self.speed * 1.5)
+                self.walking = True
+                # get angle 
+                angle = math.atan2(dis.y, dis.x)
+                if not self.timer: # if self.timer = 0
+                    if (self.flip and dis[0] < 0): # player is left of enemy, and enemy is looking left
+                        self.timer = 60 # Set a cooldown timer for shooting (300 frames = 5 seconds)
+                        self.game.sfx['shoot'].play()
+                        self.game.projectiles.append([[self.rect().centerx - 7, self.rect().centery], -2.5, 0])
+                        for i in range(4):
+                            self.game.sparks.append(Spark(self.game.projectiles[-1][0], random.random() - 0.5 + math.pi, 2 + random.random())) # getting pos from projectiles in it's list, facing left
+                    if (not self.flip and dis[0] > 0):
+                        self.game.projectiles.append([[self.rect().centerx + 7, self.rect().centery], 2.5, 0])
+                        self.timer = 60  # Set a cooldown timer for shooting (300 frames = 5 seconds)
+                        self.game.sfx['shoot'].play()
+                        for i in range(4):
+                            self.game.sparks.append(Spark(self.game.projectiles[-1][0], random.random() - 0.5, 2 + random.random())) # facing right
+        elif random.random() < 0.01: # 1 in every 6.1 seconds
+            self.walking = random.randint(30, 120)
+        
+        # Reduce timer
+        if self.timer > 0:
+            self.timer -= 1
+       
+        super().update(tilemap, movement=movement)
+
+        if movement[0] != 0 or movement[1] != 0:
+            self.set_action('run')
+        else:
+            self.set_action('idle')
+
+        if abs(self.game.player.dashing) >= 50:
+            if self.rect().colliderect(self.game.player.rect()): # if enemy hitbox collides with player
+                self.game.screenshake = max(16, self.game.screenshake)  # apply screenshake
+                self.game.sfx['hit'].play()
+                for i in range(30): # enemy death effect
+                    # on death sparks
+                    angle = random.random() * math.pi * 2 # random angle in a circle
+                    speed = random.random() * 5
+                    self.game.sparks.append(Spark(self.rect().center, angle, 2 + random.random())) 
+                    # on death particles
+                    self.game.particles.append(Particle(self.game, 'particle', self.rect().center, velocity=[math.cos(angle + math.pi) * speed * 0.5, math.sin(angle * math.pi) * speed * 0.5], frame=random.randint(0, 7)))
+                self.game.sparks.append(Spark(self.rect().center, 0, 5 + random.random())) # left
+                self.game.sparks.append(Spark(self.rect().center, math.pi, 5 + random.random())) # right
+                return True # [**]
+                
+
+    def render(self, surf, offset=(0, 0)):
+        super().render(surf, offset=offset)
+
+        if self.flip:
+            surf.blit(pygame.transform.flip(self.game.assets['bow'], True, False), (self.rect().centerx + 1 - self.game.assets['bow'].get_width() + 2 - offset[0], self.rect().centery - 8 - offset[1])) # renders the bow 
+        else:
+            surf.blit(self.game.assets['bow'], (self.rect().centerx - 1 - offset[0], self.rect().centery - 8 - offset[1]))
+
+    
+
+        
